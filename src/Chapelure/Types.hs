86b9ecd65ef760656269cc70176ca4652a1d955f
Hécate Moonlight <hecate+github@glitchbra.in>
2021-09-12 23:17:35 +0200

    Add PlainText handler

--- src/Chapelure/Types.hs
+++ src/Chapelure/Types.hs
@@ -1,96 +1,90 @@
 module Chapelure.Types where
 
 import Data.Text (Text)
+import Data.Vector.NonEmpty (NonEmptyVector)
+import GHC.Generics
+import Data.Text.Display
 import Data.Vector (Vector)
-import qualified Data.Text as T
+import Prettyprinter (Pretty)
 
 data Diagnostic = Diagnostic
   {
-    -- | Unique diagnostic code that be used to look up more information. Should be globally unique, and documented for easy searching.
-    code :: Maybe Text
-    ,
-    -- | Diagnostic severity, this may be used by the Report Handler to change the display.
-    -- If 'Nothing', the Report Handler should treat it as 'Error'.
-    severity :: Maybe Severity
-    ,
+    -- | Unique diagnostic code that be used to look up more information.
+    -- Should be globally unique, and documented for easy searching.
+    code     :: Maybe Text
+    -- | Diagnostic severity, this may be used by the Report Handler
+    -- to adapt the formatting of the diagnostic.
+  , severity :: Severity
     -- | Additional free-form text for the poor bastard at the end of it all.
-    help :: Maybe Text
-    ,
-    -- | URL to visit for a more detailed explanation. Can make use of the 'code'.
-    url :: Maybe Text
-    ,
+  , help     :: Maybe Text
+    -- | Link to visit for a more detailed explanation.
+    -- Can make use of the 'code' component.
+  , link     :: Maybe Text
     -- | Contextual snippet to provide relevant source data.
-    snippets :: Maybe (Vector Snippet)
-  } deriving (Show, Eq)
+  , snippets :: Maybe (NonEmptyVector Snippet)
+  } deriving stock (Show, Eq, Generic)
 
--- | Enum used to indicate the severity of a diagnostic. The Report Handlers will use this to
--- alter the formatting of the diagnostic.
+-- | Enum used to indicate the severity of a diagnostic.
+-- The Report Handlers will use this to adapt the formatting of the diagnostic.
 data Severity
   = Info
   | Warning
   | Error
-  deriving (Show, Eq, Enum, Bounded)
+  deriving stock (Show, Eq, Enum, Bounded)
+  deriving Display
+    via (ShowInstance Severity)
 
--- | A datatype holding a message, some source data and a span to highlight.
-data Snippet = Snippet
-  { message :: Maybe Text
-  , source :: Source
-  , context :: SourceSpan
-  }
-  deriving (Show, Eq)
-
--- | A source fragment that can be named.
-data Source = Source
-  { name :: Maybe Text
-  , content :: Text
-  }
-  deriving (Show, Eq)
-
--- | Opaque wrapper to mark an offset from the beginning of a 'Source'.
+-- | Wrapper to mark an offset from the beginning of a 'Source'.
 newtype Offset = Offset Word
-  deriving (Eq, Show)
-  deriving newtype (Num, Ord)
+  deriving stock (Eq, Show, Generic)
+  deriving newtype (Ord, Enum, Pretty)
 
 -- | Wrapper that represents a line in a Diagnostic report
 newtype Line = Line Word
-  deriving (Eq, Show)
-  deriving newtype (Num, Ord)
+  deriving stock (Eq, Show, Generic)
+  deriving newtype (Ord, Pretty)
+
+instance Display Line where
+  displayBuilder (Line line) = displayBuilder line
+
+incrementLine :: Line -> Line
+incrementLine (Line i) = Line (i + 1)
 
 -- | Wrapper that represents a column in a Diagnostic report
 newtype Column = Column Word
-  deriving (Eq, Show)
-  deriving newtype (Num, Ord)
+  deriving stock (Eq, Show, Generic)
+  deriving newtype (Ord, Pretty)
 
--- | Produce an offset according to source data, and starting line and column.
-mkOffset :: Text -> Line -> Column -> Offset
-mkOffset source l c = go (T.uncons source) 0 0 0
-  where
-    go :: Maybe (Char, Text) -> Line -> Column -> Offset -> Offset
-    go Nothing _ _ offset = offset
-    go (Just ('\n', rest)) line _ offset = go (T.uncons rest) (line + 1) 0 (offset + 1)
-    go (Just (_, _)) line col offset | (line + 1) >= l && (col + 1) >= c = offset
-    go (Just (_, rest)) line col offset = go (T.uncons rest) line (col + 1) (offset + 1)
+instance Display Column where
+  displayBuilder (Column column) = displayBuilder column
 
-data SourceSpan = SourceSpan
+incrementColumn :: Column -> Column
+incrementColumn (Column i) = Column (i + 1)
+
+-- | A datatype holding a message, some source data and a span to highlight.
+data Snippet = Snippet
   {
-    -- | Start of the span from the beginning of a 'Source'
-    start  :: Word
-  ,
-    -- | Total length of the 'SourceSpan' from 'start'
-    length :: Word
-  ,
-    -- | Absolute offset in bytes from the beginning of a 'Source'
-    offset :: Word
-  ,
-    -- | Total length in bytes of the SourceSpan
-    size   :: Word
+    -- | A location name (filename or other), and the line and columns
+    -- at which the snippet starts.
+    location :: (Text, Line, Column)
+    -- | Highlights of the source that are of interest
+  , highlights :: Maybe (NonEmptyVector Source)
+    -- | Subject that is being reported. Each member is a line.
+  , content :: Vector Text
   }
-  deriving (Eq, Show)
+  deriving stock (Show, Eq, Generic)
 
--- | Contents of a 'Source' covered by a 'SourceSpan'
-data SpanContents = SpanContents
-  { content :: Vector Text
-  , line :: Line
-  , column :: Column
+-- | A piece of source data that is shown when reporting an error.
+-- Pointers on a source are always on a single line.
+data Source = Source
+  {
+    -- | An optional label for the highlight
+    label       :: Maybe Text
+    -- | The 'Line' on which is the highlight.
+  , line        :: Line
+    -- | The 'Column' on which starts the highlight.
+  , startColumn :: Column
+    -- | The 'Column' on which ends the highlight.
+  , endColumn   :: Column
   }
-  deriving (Eq, Show)
+  deriving stock (Show, Eq, Generic)
--- test/Main.hs
+++ test/Main.hs
@@ -1,12 +1,139 @@
+{-# LANGUAGE QuasiQuotes #-}
 module Main where
 
 import Test.Hspec
+import Data.String.QQ
+import Data.Vector as Vec
+import Data.Vector.NonEmpty as NEVec
+import Data.Text as T
+
+import Chapelure.Types
+import qualified Chapelure.Handler.PlainText as PT
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
 spec = do
-    describe "Tests" $ do
-      it "Test 1" $
-        True `shouldBe` True
+    describe "PlainText Renderer" $ do
+      it "Simple reporting" $ do
+        let helpMessage = "Did you check all the types of your arguments?"
+        let diagnostic = Diagnostic { code = Just "L342"
+                                    , severity = Error
+                                    , link = Just "https://localhost:8888/help/code/L342"
+                                    , help = Just helpMessage
+                                    , snippets = Nothing
+                                    }
+        let rendered = [s|
+Diagnostic severity: Error
+ 
+Help: Did you check all the types of your arguments?
+Code: L342
+Link: https://localhost:8888/help/code/L342|]
+        PT.render diagnostic `shouldBe` rendered
+
+      it "Simple snippet rendering" $ do
+        let helpMessage = "Did you check all the types of your arguments?"
+        let snip = Snippet { location = ("Code.hs", Line 1, Column 10)
+                           , highlights = Nothing
+                           , content = Vec.fromList $ T.lines $ T.pack "add :: Int\nadd = 1 + True"
+                           }
+        let diagnostic = Diagnostic { code = Just "L342"
+                                    , severity = Error
+                                    , link = Just "https://localhost:8888/help/code/L342"
+                                    , help = Just helpMessage
+                                    , snippets = Just . NEVec.singleton $ snip
+                                    }
+        let rendered = [s|
+Diagnostic severity: Error
+ 
+-> Code.hs:1:10
+1 │ add :: Int
+2 │ add = 1 + True
+
+Help: Did you check all the types of your arguments?
+Code: L342
+Link: https://localhost:8888/help/code/L342|]
+        PT.render diagnostic `shouldBe` rendered
+
+      it "Highlighted snippet rendering with one highlight per line" $ do
+        let helpMessage = "Did you check all the types of your arguments?"
+        let highlights = NEVec.fromList [
+                   Source{ label = Just "Return type is an “Int”"
+                         , line = Line 3
+                         , startColumn = Column 7
+                         , endColumn = Column 10
+                         }
+                 , Source{ label = Just "You tried to pass a “Bool”"
+                         , line = Line 4
+                         , startColumn = Column 10
+                         , endColumn = Column 14
+                         }
+                ]
+        let snip = Snippet { location = ("Code.hs", Line 3, Column 1)
+                           , highlights = highlights
+                           , content = Vec.fromList $ T.lines $ T.pack "add :: Int\nadd = 1 + True"
+                           }
+        let diagnostic = Diagnostic { code = Just "L342"
+                                    , severity = Error
+                                    , link = Just "https://localhost:8888/help/code/L342"
+                                    , help = Just helpMessage
+                                    , snippets = Just . NEVec.singleton $ snip
+                                    }
+        let rendered = [s|
+Diagnostic severity: Error
+ 
+-> Code.hs:3:1
+3 │ add :: Int
+           ^^^ Return type is an “Int”
+4 │ add = 1 + True
+              ^^^^ You tried to pass a “Bool”
+
+Help: Did you check all the types of your arguments?
+Code: L342
+Link: https://localhost:8888/help/code/L342|]
+        PT.render diagnostic `shouldBe` rendered
+
+      it "Highlighted snippet rendering with two highlights on one line" $ do
+        let helpMessage = "Did you check all the types of your arguments?"
+        let highlights = NEVec.fromList [
+                   Source{ label = Just "Return type is “Int”"
+                         , line = Line 3
+                         , startColumn = Column 7
+                         , endColumn = Column 10
+                         }
+                 , Source{ label = Just "This takes an “Int”"
+                         , line = Line 4
+                         , startColumn = Column 8
+                         , endColumn = Column 8
+                         }
+                 , Source{ label = Just "But you passed a “Bool”"
+                         , line = Line 4
+                         , startColumn = Column 10
+                         , endColumn = Column 14
+                         }
+                ]
+        let snip = Snippet { location = ("Code.hs", Line 3, Column 1)
+                           , highlights = highlights
+                           , content = Vec.fromList $ T.lines $ T.pack "add :: Int\nadd = 1 + True"
+                           }
+        let diagnostic = Diagnostic { code = Just "L342"
+                                    , severity = Error
+                                    , link = Just "https://localhost:8888/help/code/L342"
+                                    , help = Just helpMessage
+                                    , snippets = Just . NEVec.singleton $ snip
+                                    }
+        let rendered = [s|
+Diagnostic severity: Error
+ 
+-> Code.hs:3:1
+3 │ add :: Int
+           ^^^ Return type is “Int”
+4 │ add = 1 + True
+              ^^^^ But you passed a “Bool”
+            ^ This takes an “Int”
+
+Help: Did you check all the types of your arguments?
+Code: L342
+Link: https://localhost:8888/help/code/L342|]
+        PT.render diagnostic `shouldBe` rendered
